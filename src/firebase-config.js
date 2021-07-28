40f949ad909254f7e4243ac177ac794583421dbb
maricaroj <juarezmariana18@gmail.com>
2021-07-28 16:11:16 -0300

    UPDATE/change firebase keys

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyCl_Iddqor5zwXBLwZ1-DJGDv5O9tHhCHE",
-    authDomain: "to-do-list-b40b5.firebaseapp.com",
-    projectId: "to-do-list-b40b5",
-    storageBucket: "to-do-list-b40b5.appspot.com",
-    messagingSenderId: "318093837478",
-    appId: "1:318093837478:web:8422cd166e161f6d926067"
+    apiKey: "AIzaSyBr8wGSw7eQfc0hX-rQyVXVGffVXg9FBSE",
+    authDomain: "lista-tareas-4cddd.firebaseapp.com",
+    projectId: "lista-tareas-4cddd",
+    storageBucket: "lista-tareas-4cddd.appspot.com",
+    messagingSenderId: "704403511717",
+    appId: "1:704403511717:web:c18b7b5e5eb1f61e22304c"
   };
   // Initialize Firebase
   const fb = firebase.initializeApp(firebaseConfig);
